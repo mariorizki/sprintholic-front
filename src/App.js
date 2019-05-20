c35b4419f635ae711b3b65a6705caca4b2edb75e
Mario Rizki <mariorizki07@gmail.com>
2019-05-21 06:42:15 +0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path={`/`} component={Login} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
